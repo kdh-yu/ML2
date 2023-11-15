50a75bb605c4457aebecfe33d9656a3432b647fa
tt-adisoh <ktx030430@naver.com>
2023-11-15 17:37:59 +0900

    ,

--- Assignment/Assignment#3/data.py
+++ Assignment/Assignment#3/data.py
@@ -1,39 +1,37 @@
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from PIL import Image
 import os
 import pyheif
+from PIL import Image
+from torch.utils.data import Dataset
 
-class CustomHEICImageDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_paths = [file for file in os.listdir(root_dir) if file.endswith('.heic')]
-
+class CustomDataset(Dataset):
+    def __init__(self, folder_path):
+        self.image_paths = self._find_heic_images(folder_path)
+        
     def __len__(self):
         return len(self.image_paths)
-
+    
     def __getitem__(self, idx):
-        heic_path = os.path.join(self.root_dir, self.image_paths[idx])
-
-        # HEIC 이미지를 JPEG로 변환
-        output_jpg_path = os.path.splitext(heic_path)[0] + '.jpg'
-        self.convert_heic_to_jpg(heic_path, output_jpg_path)
-
-        # JPEG 이미지를 PIL 이미지로 로드
-        image = Image.open(output_jpg_path)
-
-        # 변환 적용
-        if self.transform:
-            image = self.transform(image)
-
-        return image
-
-    def convert_heic_to_jpg(self, heic_path, output_jpg_path):
+        heic_path = self.image_paths[idx]
+        jpg_path = self._get_output_path(heic_path)
+        self._convert_heic_to_jpg(heic_path, jpg_path)
+        return jpg_path
+    
+    def _find_heic_images(self, folder_path):
+        heic_images = []
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith('.heic'):
+                    heic_images.append(os.path.join(root, file))
+        return heic_images
+    
+    def _get_output_path(self, heic_path):
+        return os.path.splitext(heic_path)[0] + '.jpg'
+    
+    def _convert_heic_to_jpg(self, heic_path, output_jpg_path):
         heif_file = pyheif.read(heic_path)
         image = Image.frombytes(
-            heif_file.mode, 
-            heif_file.size, 
+            heif_file.mode,
+            heif_file.size,
             heif_file.data,
             "raw",
             heif_file.mode,
